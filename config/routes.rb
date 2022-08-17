8ce820aa040c0fd5550f62c9cdb226dab0bbf672
nazanin bos <nazanin.manfaz@gmail.com>
2022-08-17 13:42:13 +0200

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :restaurants
+  
 end
+
+
+# A restaurant must have a name, an address and a category.
+# A restaurant’s category must belong to this fixed list: ["chinese", "italian", "japanese", "french", "belgian"].
+# When a restaurant is destroyed, all of its reviews must be destroyed as well.
+# A review must belong to a restaurant.
+# A review must have a content.
+# A review must have a rating.
+# A review’s rating must be a number between 0 and 5.
+# A review’s rating must be an integer. For example, a review with a rating of 2.5 should be invalid!
